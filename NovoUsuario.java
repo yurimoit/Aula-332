29ac84a8ef5a979a91c4c53f12f3a85599902634
yurimoit <113135569+yurimoit@users.noreply.github.com>
2022-09-30 17:44:55 -0300

    Add files via upload

---
+++ NovoUsuario.java
@@ -0,0 +1,30 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+
+public class NovoUsuario{
+   public static void main(String[] args) {
+	
+	EntityManagerFactory emf= Persistence
+			.createEntityManagerFactory("exercicios-jpa");   
+	EntityManager em=emf.createEntityManager();  
+	
+	Usuario novoUsuario=new Usuario("Carlos","Carlos@lanche");
+	
+	em.getTransaction().begin();
+	em.persist(novoUsuario);
+	em.getTransaction().commit();
+	
+
+	
+	em.close();
+	emf.close();
+	
+	
+}
+}
